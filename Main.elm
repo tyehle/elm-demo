3207a39ff09f7f023bf246e212e91d95b2b3077c
Tobin Yehle <tobinyehle@gmail.com>
2016-04-20 23:57:42 -0600

    make the bouncing ball demo

--- Main.elm
+++ Main.elm
@@ -3,27 +3,47 @@ import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Time exposing (..)
 
+type alias State = { position : (Float, Float)
+                   , velocity : (Float, Float)
+                   }
+
+width = 800
+height = 800
+size = 20
 
 main : Signal Element
 main =
-  Signal.map clock (every (second / 60))
+  let
+    start = {position = (-50,0), velocity = (0.2,-0.1)}
+    total = Signal.foldp bounce start (fps 60)
+  in
+    Signal.map ball total
 
 
-clock : Float -> Element
-clock t =
-  collage 800 800
-    [ filled lightGrey (ngon 64 220)
-    , outlined (solid grey) (ngon 64 220)
-    , hand orange 200 t
-    , hand charcoal 200 (t/60)
-    , hand charcoal 120 (t/720)
+ball : State -> Element
+ball s =
+  collage width height
+    [ filled charcoal (circle size)
+        |> move (fst s.position, snd s.position)
     ]
 
 
-hand : Color -> Float -> Float -> Form
-hand clr len time =
+bounce : Time -> State -> State
+bounce t s =
   let
-    angle = degrees (90 - 6 * inSeconds time)
+    (newX, newVX) = updatePosition t (fst s.position, fst s.velocity) (width/2 - size)
+    (newY, newVY) = updatePosition t (snd s.position, snd s.velocity) (height/2 - size)
   in
-    segment (0,0) (fromPolar (len,angle))
-      |> traced (solid clr)
+    { position = (newX, newY)
+    , velocity = (newVX, newVY)
+    }
+
+updatePosition : Time -> (Float, Float) -> Float -> (Float, Float)
+updatePosition t (x, v) bound =
+  let
+    projected = x + t*v
+    extra     = (abs projected) - bound
+    newV      = if extra <= 0 then v else -v
+    newX      = if extra <= 0 then projected else projected + 2*newV*extra
+  in
+    (newX, newV)
