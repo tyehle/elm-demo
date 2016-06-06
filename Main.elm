d92c7f6fb6eddc3bf1554a9d84c5c5d66c1452a8
Tobin Yehle <tobinyehle@gmail.com>
2016-06-06 14:04:26 -0600

    basic working example for elm 0.17

--- Main.elm
+++ Main.elm
@@ -1,7 +1,7 @@
 import Color exposing (..)
-import Graphics.Collage exposing (..)
-import Graphics.Element exposing (..)
-import Time exposing (..)
+import Html exposing (Html, text)
+import Html.App
+--import Time exposing (..)
 
 type alias State = { position : (Float, Float)
                    , velocity : (Float, Float)
@@ -9,59 +9,96 @@ type alias State = { position : (Float, Float)
                    , color : Color
                    }
 
-width : Float
-width = 800
+--width : Float
+--width = 800
 
-height : Float
-height = 800
-
-
-main : Signal Element
-main =
-  let
-    start = [ { position = (-50,0), velocity = (0.2,-0.1), size = 20, color = darkPurple }
-            , { position = (50,0),  velocity = (0.2,-0.4), size = 10, color = charcoal }
-            ]
-    accumulated = Signal.foldp bounce start (fps 60)
-  in
-    Signal.map draw accumulated
+--height : Float
+--height = 800
 
 
-ball : State -> Form
-ball s =
-  filled s.color (circle s.size)
-    |> move (fst s.position, snd s.position)
+
+-- MODEL
+
+type alias Model = ()
 
 
-draw : List State -> Element
-draw states =
-  collage (round width) (round height)
-    (filled darkGrey (rect width height) :: List.map ball states)
+
+-- UPDATE
+
+type Msg = Reset
+
+update : Msg -> Model -> Model
+update _ = identity
 
 
-bounce : Time -> List State -> List State
-bounce t states =
-  let updater = updateState t in
-    List.map updater states
+
+-- VIEW
+
+view : Model -> Html msg
+view _ = text "Hello World"
 
 
-updateState : Time -> State -> State
-updateState t s =
-  let
-    (newX, newVX) = updatePosition t (fst s.position, fst s.velocity) (width/2 - s.size)
-    (newY, newVY) = updatePosition t (snd s.position, snd s.velocity) (height/2 - s.size)
-  in
-    { s | position = (newX, newY)
-        , velocity = (newVX, newVY)
-    }
+
+-- MAIN
+
+main : Html msg
+main = text "hello world"
 
 
-updatePosition : Time -> (Float, Float) -> Float -> (Float, Float)
-updatePosition t (x, v) bound =
-  let
-    projected = x + t*v
-    extra     = (abs projected) - bound
-    newV      = if extra <= 0 then v else -v
-    newX      = if extra <= 0 then projected else projected + 2*newV*extra
-  in
-    (newX, newV)
+
+--draw : (Int, Int) -> Element
+--draw (width, height) = 
+--  let
+--    background = filled charcoal (rect (toFloat width) (toFloat height))
+--  in
+--    collage width height [background]
+
+--main : Signal Element
+--main =
+--  let
+--    start = [ { position = (-50,0), velocity = (0.2,-0.1), size = 20, color = darkPurple }
+--            , { position = (50,0),  velocity = (0.2,-0.4), size = 10, color = charcoal }
+--            ]
+--    accumulated = Signal.foldp bounce start (fps 60)
+--  in
+--    Signal.map draw accumulated
+
+
+--ball : State -> Form
+--ball s =
+--  filled s.color (circle s.size)
+--    |> move (fst s.position, snd s.position)
+
+
+--draw : List State -> Element
+--draw states =
+--  collage (round width) (round height)
+--    (filled darkGrey (rect width height) :: List.map ball states)
+
+
+--bounce : Time -> List State -> List State
+--bounce t states =
+--  let updater = updateState t in
+--    List.map updater states
+
+
+--updateState : Time -> State -> State
+--updateState t s =
+--  let
+--    (newX, newVX) = updatePosition t (fst s.position, fst s.velocity) (width/2 - s.size)
+--    (newY, newVY) = updatePosition t (snd s.position, snd s.velocity) (height/2 - s.size)
+--  in
+--    { s | position = (newX, newY)
+--        , velocity = (newVX, newVY)
+--    }
+
+
+--updatePosition : Time -> (Float, Float) -> Float -> (Float, Float)
+--updatePosition t (x, v) bound =
+--  let
+--    projected = x + t*v
+--    extra     = (abs projected) - bound
+--    newV      = if extra <= 0 then v else -v
+--    newX      = if extra <= 0 then projected else projected + 2*newV*extra
+--  in
+--    (newX, newV)
